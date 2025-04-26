6fe46316b4a8ba457d02d6f770ab5fe038f6932b
Maeevick <maeevick@gmail.com>
2025-04-26 23:19:51 +0200

    feat: enhanced hello trixlab

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, TrixLab!");
+    let workshop_name = "TrixLab";
+    let version = 0.1;
+    
+    println!("Welcome to the {} workshop!", workshop_name);
+    println!("Version {} - First enchantment activated", version);
+    println!("Ready to create magico-mechanical wonders!");
 }
