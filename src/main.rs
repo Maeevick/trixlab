c76e5a5cf2fdeff6c7ce662a834a1fc68ab67edd
Maeevick <maeevick@gmail.com>
2025-04-26 23:22:14 +0200

    feat: println macro

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
     let workshop_name = "TrixLab";
     let version = 0.1;
+    let year = 2025;
+    let is_experimental = true;
     
-    println!("Welcome to the {} workshop!", workshop_name);
-    println!("Version {} - First enchantment activated", version);
-    println!("Ready to create magico-mechanical wonders!");
+    println!("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    println!("Workshop name: {}", workshop_name);
+    println!("Version: {:.1}", version);  // Formatting to 1 decimal place
+    println!("Year: {year}");  // Direct use of variable name
+    println!("Experimental mode: {}", if is_experimental { "ACTIVATED" } else { "DEACTIVATED" });
+    println!("{:-^40}", " WORKSHOP JOURNAL ");  // Centered with dash filling
+    println!("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 }
